78a2a54c82db9b68cdd0ba6179442a055855ff04
toran billups <toranb@gmail.com>
2013-07-28 23:07:05 -0500

    added custom serializer classes

--- website/views.py
+++ website/views.py
@@ -1,6 +1,18 @@
-from website.models import Session
+from website.models import Session, Speaker
 from django.views.generic.list import ListView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework import serializers
+
+class SpeakerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Speaker
+
+class SessionSerializer(serializers.ModelSerializer):
+    speakers = SpeakerSerializer()
+
+    class Meta:
+        model = Session
 
 class IndexView(ListView):
     template_name = 'index.html'
@@ -10,6 +22,8 @@ class IndexView(ListView):
 
 class SessionAPIListView(ListCreateAPIView):
     model = Session
+    serializer_class = SessionSerializer
 
 class SessionAPIRetrieveView(RetrieveUpdateAPIView):
     model = Session
+    serializer_class = SessionSerializer
